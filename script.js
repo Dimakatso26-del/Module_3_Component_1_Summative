37ff491a5b8d709e4e2fe6d05480de833849ee6a
Dimakatso26-del <mosimadimakatso635@gmail.com>
2026-07-21 10:52:16 +0200

    Component 1

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+
 // ===========================================
 // FETCH HTML ELEMENTS
 // SECTION A
 // ===========================================
 
 const runTasksButton = document.getElementById("runTasksButton");
-const outputElement = "";
+const outputElement = document.querySelector("runTasksButton");
 
 // ===========================================
 // DATA
@@ -22,31 +23,43 @@ export const marks = [45, 60, 85, 30, 95];
 export function addMark() {
     //add 70 to the end of the marks array
     // Return the updated marks array
+    let marks = marks.add(function(marks));
+    console.log(marks)
 }
 
 // Task 2
 export function getMarksOver60() {
     // Return marks greater than 60
+    let marks = marks.filter(function(marks));
+    return marks > 60
 }
-
+console.log(marks)
 // Task 3
 export function increaseMarksBy5() {
     // Return a new array where each mark is increased by 5
+    let marksforEach = marks.forEach(function(marks));
+    return mark + 5
 }
-
+console.log(marks)
 // Task 4
 export function getTotalMarks() {
     // Return the total of all marks
+    let totalmarks = marks.add(function(marks)); 
 }
-
+console.log(totalmarks)
 // Task 5
 export function findFirstMarkOver80() {
     // Return the first mark greater than 80
-}
+    let marks = marks.includes(function(mark));
+    return marks > 80
+    
 
+}
+console.log(marks)
 // Task 6
 export function sortMarksLowestToHighest() {
     // Return marks sorted from lowest to highest
+    
 }
 
 // ===========================================
@@ -81,7 +94,7 @@ export function runTasks() {
     displayResult("Task 6", sortMarksLowestToHighest());
 
 }
-
+addEventListener = document.querySelector(button id)
 // ===========================================
 // ADD EVENT LISTENER
 // SECTION A 
